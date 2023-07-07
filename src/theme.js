4557ba5d5e70e9b03b5ce5e9e5dcb7634a1ebb96
Soniq7 <ssoniq7@gmail.com>
2023-07-07 14:01:46 +0100

    theme.js created and imported to index.js

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+
+export const theme = {
+    color: {
+        white: "#ffffff",
+        black: "#000000",
+        lightBlack: "#222",
+        grey: "#ddd",
+        lightGrey: "#eee",
+        darkerGrey: "#ccc",
+        teal: "#008080",
+        lightTeal: "#019898",
+        lighterTeal: "#00b3b3",
+        crimson: "#dc143c",
+        lightCrimson: "#ec365b",
+        lighterCrimson: "#ef4e6e",
+        green: "#008000",
+        lightGreen: "#009900",
+        lighterGreen: "#00cc00",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
