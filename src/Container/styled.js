40cf9bd19d8dad524842864f2cd3317c0cfa2a97
Soniq7 <ssoniq7@gmail.com>
2023-07-13 18:35:23 +0100

    Component Container changed to styled-components

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-export const Main = styled.main`
+const Main = styled.main`
   max-width: 1000px;
   margin: 0 auto;
   padding: 20px;
 `;
+
+const Container = ({ children }) => (
+  <Main>
+    {children}
+  </Main>
+);
+
+export default Container;
